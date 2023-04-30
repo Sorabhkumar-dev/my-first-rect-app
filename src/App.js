699512779e2e5c55d188ac95060cb295e5a620be
Sorabh kumar <96188190+Sorabhkumar-dev@users.noreply.github.com>
2023-04-30 10:46:19 +0530

    :added react router six
    Chore:added react router six

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import "./App.css";
+import { useState } from "react";
+import Alert from "./components/Alert";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 
 function App() {
+  const [alert,setAlert] = useState(null)
+  
+  
+  const showAlert = (alertType,messageType) => {
+    setAlert({
+      type:alertType,
+      message:messageType
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 3000);
+  }
+
   return (
     <>
       <Navbar title="Faster Corp" home="Home" about="About us" contact="Contact us" />
+      <Alert alert = {alert}/>
       <div className="container my-3">
-      <TextForm  heading = "Enter Text here"/>
+      <TextForm  heading = "Enter Text here" showAlert = {showAlert}/>
       </div>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 // import $ from  'jquery';
 // import Popper from 'popper.js';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import AboutUs from "./components/AboutUs";
+import ContactUs from "./components/ContactUs";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Navbar
+            title="Faster Corp"
+            home="home"
+            about="About us"
+            contact="Contact us"
+          />
+        }>
+        <Route path="home" element={<TextForm />} />
+        <Route path="about" element={<AboutUs />} />
+        <Route path="contact" element={<ContactUs />} />
+      </Route>
+    </Routes>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
